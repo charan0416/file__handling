7d7b42a2eb57fa9f548b104bb8d28227994b4557
charan0416 <charanbaru169@gmail.com>
2025-03-24 10:43:08 +0530

    filehandling

---
+++ file_handling.py
@@ -0,0 +1,59 @@
+# file = open('charan.txt', 'r')
+# content = file.read()
+# print(content)
+# file.close()
+from certifi import contents
+
+# file = open('charan.txt', 'r')
+# line = file.readline()
+# print(line)  # Prints the first line
+# file.close()
+
+#
+# file = open('charan.txt', 'r')
+# lines = file.readlines()
+# print(lines)  # Prints a list of lines in the file
+# file.close()
+
+
+# file = open('charan.txt', 'w')
+# file.write("cherry charan cherry.")
+# file.close()
+#
+# file = open('charan.txt', 'r')
+# content = file.read()
+# print(content)
+# file.close()
+
+#
+# lines = ["First line\n", "Second line\n", "Third line\n"]
+# file = open('charan.txt', 'w')
+# file.writelines(lines)
+# file.close()
+#
+# file = open('charan.txt', 'r')
+# content = file.read()
+# print(content)
+# file.close()
+
+
+# file = open('example.txt', 'a')
+# file.write("\nThis line will be appended.")
+# file.close()
+#
+#
+#
+# file = open('charan.txt', 'r')
+# content = file.read()
+# print(content)
+# file.close()
+
+# file = open('charan.txt', 'r')
+# file.seek(5)
+# content = file.read()
+# print(content)
+# file.close()
+
+with open('', 'rb') as file:
+    content = file.read()
+    # Process the binary content
